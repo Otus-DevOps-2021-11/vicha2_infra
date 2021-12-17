026ac3f3bd540bf97cc045101fe339c444b5edf1
Vyacheslav Eskin <vicha2@inbox.ru>
2021-12-17 17:38:45 +0000

    ssh and vpn bastion

---
+++ setupvpn.sh
@@ -0,0 +1,16 @@
+cat <<EOF> setupvpn.sh
+#!/bin/bash
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt-get update
+apt-get install mongodb-server -y
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+echo "deb http://repo.pritunl.com/stable/apt focal main" | tee /etc/apt/sources.list.d/pritunl.list
+apt-get update
+apt-get install pritunl -y
+systemctl start pritunl
+systemctl enable pritunl
+EOF
