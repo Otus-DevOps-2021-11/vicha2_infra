e41e51bb27c13cf56889398b8c09662c73768650
Vyacheslav Eskin <vicha2@inbox.ru>
2022-01-02 03:26:56 -0500

    config version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,10 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version  = ">= 0.35"
     }
   }
+  required_version = ">= 0.12"
 }
 
 provider "yandex" {
