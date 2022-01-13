2aff60e0de0cce60c6041751c77d08746bd5027d
Vyacheslav Eskin <vicha2@inbox.ru>
2022-01-13 02:11:30 -0500

    delete key backend

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -4,8 +4,6 @@ terraform {
     bucket                      = "bucket-stage-state"
     region                      = "ru-central1"
     key                         = "state.tfstate"
-    access_key                  = "WJ-3213oH-SvJpvkMG9i"
-    secret_key                  = "Tc3Fvj321-m8RsmZT5hoIynGfM0xgfnB_Ix3od13"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
